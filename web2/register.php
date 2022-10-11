f6f71e9f87d7f103680757aed65365ede511c5da
Mutooro <98326730+Mutooro@users.noreply.github.com>
2022-10-11 18:13:34 +0300

    Add files via upload

---
+++ web2/register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+     <head>
+     <title></title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="styles.css"/>
+     </head>
+    <body>
+       <p style="text-align: left"> Welcome <?php echo $_POST["email"]; ?>
+        <br>
+       Your account has been created successfully.Please click <a href="login.html">&nbsphere</a> to login</p>
+    </body>
+</html>
